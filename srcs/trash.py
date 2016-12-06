a4e7f365afe88da3c35f9f81556b08772ba199bd
PTank <akazian@student.42.fr>
2016-12-06 22:03:23 +0100

    basic work

--- srcs/trash.py
+++ srcs/trash.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
-from path import path
-import os
+from pathlib import Path
 
 
 class Trash():
@@ -8,3 +7,38 @@ class Trash():
     def __init__(self, path=None):
         self.path = path
 
+    def list_files(self, list_file=None, size=False):
+        total = 0
+        if not list_file:
+            l = Path(self.path + '/files').iterdir()
+        else:
+            l = map(lambda x: Path(self.path + '/files/' + x), list_file)
+        for i in l:
+            try:
+                s = i.name + ['', '\t' + str(i.lstat().st_size)][bool(size)]
+                total += i.lstat().st_size
+                print(s)
+            except Exception as e:
+                print(e)
+        if size:
+            print("Total:\t%d" % total)
+
+    def clean(self, list_file=None, path=None):
+        if not path:
+            path = self.path + '/files'
+        if not list_file:
+            l = Path(path).iterdir()
+        else:
+            l = map(lambda x: Path(path + x), list_file)
+        for i in l:
+            try:
+                if i.is_dir():
+                    self.clean(path=(str(i)))
+                    i.rmdir()
+                else:
+                    i.unlink()
+            except Exception as e:
+                print(e)
+
+    def __str__(self):
+        return "%s" % (self.path)
--- srcs/trashtalk.py
+++ srcs/trashtalk.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 from __future__ import print_function
 import argparse
-from trash import Trash
+from os import getlogin
+from pathlib import Path
+from make_path import get_media_trash, get_home_trash
 
 
 def parse_option():
@@ -29,12 +31,13 @@ def parse_option():
     # TRASH OPTION
     option.add_argument('-p', action='store_true', default=False,
                         help="print trash path")
+    option.add_argument('-f', '--files', action='store', nargs='*',
+                        help="select file in trash")
     option.add_argument('-l', action='store_true', default=False,
                         help="list file in trash")
-    option.add_argument('-s', action='store', nargs='*',
-                        help=("print size, if -du and "
-                              "-cl was select du is run before -rm"))
-    option.add_argument('-cl', '--clean', action='store', nargs='*',
+    option.add_argument('-s', action='store_true',
+                        help=("print size"))
+    option.add_argument('-cl', '--clean', action='store_true',
                         help="clean file, or without file all")
     option.add_argument('-rm', action='store', nargs='*',
                         help="move file to selected trash")
@@ -42,9 +45,35 @@ def parse_option():
 
 
 def main():
+    trashs = []
     options = parse_option()
-    trash = Trash()
-    print(trash.trash_list)
+    if not options.u and not options.au:
+        options.u = [getlogin()]
+    elif options.au:
+        options.u = [i.name for i in Path('/home').iterdir()]
+    if not options.trash:
+        for user in options.u:
+            if options.a or not options.am:
+                trashs.append(get_home_trash(user))
+            if options.am or options.a:
+                trashs.append(get_media_trash(user))
+    else:
+        for media in options.trash:
+            for user in options.u:
+                t = get_media_trash(user, [media])
+                if t:
+                    trashs.append(t)
+    for t in trashs:
+        for trash in t:
+            if not trash:
+                continue
+            if options.p or (
+                    not options.l and not options.s and not options.clean):
+                print("%s: %s" % (trash[0], str(trash[1])))
+            if options.l or options.s:
+                trash[1].list_files(options.files, options.s)
+            if options.clean:
+                trash[1].clean(options.files)
 
 if __name__ == "__main__":
     main()
