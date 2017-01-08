7134c24693d43f3bb6058d357ba016d9b832045d
PTank <akazian@student.42.fr>
2017-01-08 01:04:22 +0100

    forget python2 get long for long int

--- trashtalk/tools.py
+++ trashtalk/tools.py
@@ -1,7 +1,7 @@
 
 
 def human_readable_from_bytes(num):
-    if type(num) is not int:
+    if not num or type(num) is str:
         return num
     for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
         if abs(num) < 1024:
